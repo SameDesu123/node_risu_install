5fcacbe8840c6ea7a15127e1b4dd8c8a70d56375
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2024-06-25 16:58:14 +0900

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -6,35 +6,8 @@ pkg update # termux 기본 패키지 확인
 
 # Termux 패키지 설치
 pkg install -y \
-  apt autoconf automake bash bc binutils-bin binutils-libs binutils \
-  bison brotli build-essential bzip2 c-ares ca-certificates-java \
-  ca-certificates cgif clang cmake command-not-found coreutils curl \
-  dash debianutils dialog diffutils dos2unix dpkg ed fftw findutils \
-  flex fontconfig freetype fribidi gawk gdbm gdk-pixbuf giflib git \
-  glib gnupg golang gperf gpgme gpgmepp gpgv grep gzip harfbuzz \
-  imagemagick imath inetutils jq jsoncpp krb5 ldns less \
-  libandroid-execinfo libandroid-glob libandroid-posix-semaphore \
-  libandroid-shmem libandroid-spawn libandroid-support \
-  libandroid-utimes libaom libarchive libassuan libbz2 libc++ \
-  libcairo libcap-ng libcompiler-rt libcrypt libcurl libdav1d libdb \
-  libde265 libedit libevent libexif libexpat libffi libgcrypt libgmp \
-  libgnutls libgpg-error libgraphite libheif libiconv libicu libidn2 \
-  libimagequant libjpeg-turbo libjxl libksba libllvm libltdl liblz4 \
-  liblzma liblzo libmd libmpfr libnettle libnghttp2 libnghttp3 libnpth \
-  libnspr libnss libpixman libpng librav1e libresolv-wrapper librsvg \
-  libsmartcols libsqlite libssh2 libtiff libtirpc libtool libunbound \
-  libunistring libuuid libuv libvips libwebp libx11 libx265 libxau \
-  libxcb libxdmcp libxext libxft libxi libxml2 libxrender libxtst \
-  littlecms lld llvm lsof m4 make nano ncurses-ui-libs ncurses-utils \
-  ncurses ndk-sysroot net-tools nodejs-lts oniguruma openexr \
-  openjdk-17-x openjdk-17 openjpeg openssh-sftp-server openssh \
-  openssl pango patch pcre2 perl pinentry pkg-config poppler procps \
-  psmisc pup python-ensurepip-wheels python-pip python readline \
-  resolv-conf rhash sed tar termux-am-socket termux-am termux-auth \
-  termux-exec termux-keyring termux-licenses termux-tools ttf-dejavu \
-  unzip util-linux wget x11-repo xorg-util-macros xorgproto xxhash \
-  xz-utils zlib zstd
-
+  build-essential x11-repo xorgproto golang libvips \
+  python git nodejs-lts install binutils
 
 ## https://github.com/Automattic/node-canvas/issues/1447#issuecomment-506507145
 
